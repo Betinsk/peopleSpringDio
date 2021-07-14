6f1ca23d0267981c5c5a7e5f89a37839c31591fa
Betinsk <gmdust@hotmail.com>
2021-07-14 18:47:06 -0300

    Atualizacao

---
+++ personapi/src/main/java/com/digitalinnovation/personapi/dto/request/PersonDTO.java
@@ -0,0 +1,43 @@
+package com.digitalinnovation.personapi.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonDTO {
+
+    private Long id;
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String FirtsName;
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String lastName;
+
+
+    @NotEmpty
+    @Size(min = 11, max = 11)
+    private String cpf;
+
+    private String birthDate;
+
+    @Valid
+    @NotEmpty
+    private List<PhoneDTO> phones;
+
+
+
+
+}
--- personapi/src/main/java/com/digitalinnovation/personapi/service/PersonService.java
+++ personapi/src/main/java/com/digitalinnovation/personapi/service/PersonService.java
@@ -1,26 +1,56 @@
 package com.digitalinnovation.personapi.service;
 
 import com.digitalinnovation.personapi.dto.MessageResponseDTO;
+import com.digitalinnovation.personapi.dto.request.PersonDTO;
 import com.digitalinnovation.personapi.entity.Person;
+import com.digitalinnovation.personapi.exception.PersonNotFoundException;
+import com.digitalinnovation.personapi.mapper.PersonMapper;
 import com.digitalinnovation.personapi.repository.PersonRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+
 public class PersonService {
 
     private PersonRepository personRepository;
 
-    @Autowired
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
-    public MessageResponseDTO CreatePerson(Person person) {
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO.builder()
+    public MessageResponseDTO CreatePerson(PersonDTO personDTO) {
+        Person personToSave = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personToSave);
+        return MessageResponseDTO
+                .builder()
                 .message("Created person with ID = "+ savedPerson.getId())
                 .build();
     }
 
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper:: toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+       Person person = verifyIfExists(id);
+       return personMapper.toDTO(person);
+    }
+
+    public void delete(Long id) throws PersonNotFoundException {
+        verifyIfExists(id);
+        personRepository.deleteById(id);
+    }
+
+    public Person verifyIfExists(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
+    }
 }
